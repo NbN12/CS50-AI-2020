00f35d5281eb3b6df9c62ef7419e442274fadd66
Jack Wonder <babyfacedeveloper@gmail.com>
2021-05-07 08:41:52 +0700

    Temp solution for traffic

--- Project-5/Traffic/traffic.py
+++ Project-5/Traffic/traffic.py
@@ -5,6 +5,10 @@ import sys
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.python.keras.layers.core import Flatten
+from tensorflow.python.keras.layers.pooling import MaxPooling2D
 
 EPOCHS = 10
 IMG_WIDTH = 30
@@ -58,7 +62,17 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images, labels = [], []
+    for i in range(NUM_CATEGORIES):
+        curDir = os.path.join(data_dir, str(i))
+        for imageFile in os.listdir(curDir):
+            img = cv2.imread(os.path.join(curDir, imageFile))
+            resized_img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            temp = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+            images.append(temp)
+            labels.append(i)
+
+    return (images, labels)
 
 
 def get_model():
@@ -67,7 +81,16 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(10, (3, 3), activation="relu",
+                               input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+    model.compile(optimizer="adam",
+                  loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
 
 
 if __name__ == "__main__":
